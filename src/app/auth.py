7a9ab91942c87adf7251e171c36c220eed67a440
djccnt15 <djccnt15@gmail.com>
2023-06-18 17:29:38 +0900

    create get_current_user for auth in app

---
+++ src/app/auth.py
@@ -0,0 +1,34 @@
+from fastapi import HTTPException, Depends
+from fastapi.security import OAuth2PasswordBearer
+from sqlalchemy.ext.asyncio import AsyncSession
+from starlette import status
+from jose import jwt, JWTError
+
+from settings.config import auth_config
+from settings.database import get_db
+from src.crud.common.user import get_user
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl='/api/user/login')
+
+
+async def get_current_user(
+        token: str = Depends(oauth2_scheme),
+        db: AsyncSession = Depends(get_db)
+):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail='Could not validate credentials',
+        headers={'WWW-Authenticate': 'Bearer'},
+    )
+    try:
+        payload = jwt.decode(token, auth_config.secret_key, algorithms=[auth_config.algorithm])
+        username: str = payload.get('sub')  # type: ignore
+        if username is None:
+            raise credentials_exception
+    except JWTError:
+        raise credentials_exception
+    else:
+        user = await get_user(db, username)
+        if user is None:
+            raise credentials_exception
+        return user
