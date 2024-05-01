a34f13eef4f809985a4e5ca74c8c61341276671c
Jay.D <djccnt15@gmail.com>
2024-05-01 11:37:48 +0900

    update main app and configurations

--- main.py
+++ main.py
@@ -1,72 +1,31 @@
-import json
-from datetime import datetime
-
 from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
-from starlette.responses import RedirectResponse, PlainTextResponse
-from starlette.requests import Request
 
-from conf.config import get_config, mode, dir_config
-from conf.database import engine
-from src.routes import router
-from src.crud import create_log
+from src import configs
+from src.common.exception import handlers as exception_handler
 
-metadata = get_config()['SWAGGER']
+config = configs.config.fastapi
 
-with open(dir_config / metadata.get('description')) as f:
-    description = f.read()
+app = FastAPI(**config)
 
-with open(dir_config / metadata.get('tags_metadata')) as f:
-    tags_metadata = json.load(f)['tags']
-
-app = FastAPI(
-    title=metadata.get('title'),
-    version=metadata.get('version'),
-    contact={
-        'name': metadata.get('name'),
-        'url': metadata.get('url'),
-        'email': metadata.get('email')
-    },
-    license_info={
-        'name': metadata.get('license_name'),
-        'url': metadata.get('license_url')
-    },
-    description=description,
-    openapi_tags=tags_metadata
-)
-
-origins = get_config()['CORSLIST'].get(mode).split()  # get CORS allow list
+# Exception Handler
+exception_handler.add_handlers(app=app)
 
 app.add_middleware(  # allow CORS credential
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=config.cors_orign,
     allow_credentials=True,
-    allow_methods=['*'],
-    allow_headers=['*'],
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
-# Routers
-app.include_router(router, prefix='/api')
+if __name__ == "__main__":
+    import uvicorn
 
+    uvicorn_config = configs.config.uvicorn
 
-# @app.middleware('http')
-async def logger(request: Request, call_next):
-    log = {
-        'req_url': str(request.url),
-        'req_method': str(request.method),
-        'param_query': str(dict(request.query_params)),
-        'param_path': str(request.path_params),
-        'client': str(request.client)
-    }
-    await create_log(engine, datetime.now(), str(log))
-    return await call_next(request)
-
-
-@app.get('/robots.txt', response_class=PlainTextResponse)
-def robots():
-    return 'User-agent: *\nAllow: /'
-
-
-@app.get('/', response_class=RedirectResponse)  # temporal index page redirect to swagger
-async def root():
-    return '/docs'
+    uvicorn.run(
+        # app="main:app",  # use this line when reload config is true
+        app=app,
+        **uvicorn_config
+    )
--- src/__init__.py
+++
@@ -1,5 +0,0 @@
-from .app import *
-from .crud import *
-from .endpoints import *
-from .models import *
-from .schemas import *
