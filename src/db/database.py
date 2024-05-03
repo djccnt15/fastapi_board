597700084691e9352f71a300cf8afb8ab8841b32
Jay.D <djccnt15@gmail.com>
2024-05-04 00:27:31 +0900

    create database pool and dependency injection

---
+++ src/db/database.py
@@ -0,0 +1,20 @@
+from sqlalchemy.engine import URL
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+
+from src import configs
+
+config = configs.config.db
+
+DB_URL = URL.create(**config.url)
+engine = create_async_engine(
+    url=DB_URL,
+    **config.engine,
+)
+
+
+async def get_db():
+    db = AsyncSession(bind=engine)
+    try:
+        yield db
+    finally:
+        await db.close()
