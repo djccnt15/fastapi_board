01a6a47fa20cfa4f1a7ce4c0ebd728776b97de73
Jay.D <djccnt15@gmail.com>
2024-05-16 23:01:57 +0900

    update business domain api router include board domain api to router

--- src/routes/api.py
+++ src/routes/api.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter
 
+from ..domain.board.endpoint import board_controller
 from ..domain.user.endpoint import user_controller
 from .enums.tag import RouterTagEnum
 
@@ -8,4 +9,9 @@ router = APIRouter(prefix="/api")
 router.include_router(
     user_controller.router,
     tags=[RouterTagEnum.USER],
+)
+
+router.include_router(
+    board_controller.router,
+    tags=[RouterTagEnum.BOARD],
 )
