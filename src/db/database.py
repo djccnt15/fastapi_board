928af0f92db0f7c5996b538758efa94f50739a81
Jay.D <djccnt15@gmail.com>
2024-05-31 23:17:54 +0900

    update database module create with_db generator in database for none api function

--- src/db/database.py
+++ src/db/database.py
@@ -1,3 +1,5 @@
+from contextlib import asynccontextmanager
+
 from sqlalchemy.engine import URL
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 
@@ -13,6 +15,15 @@ engine = create_async_engine(
 
 
 async def get_db():
+    db = AsyncSession(bind=engine)
+    try:
+        yield db
+    finally:
+        await db.close()
+
+
+@asynccontextmanager
+async def with_db():
     db = AsyncSession(bind=engine)
     try:
         yield db
